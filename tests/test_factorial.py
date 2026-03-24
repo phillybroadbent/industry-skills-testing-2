f428544ac96e63c206f1e287e8e8f4308b5ef664
phillybroadbent <phillybroadbent@gmail.com>
2026-03-24 10:16:19 +0000

    Add tests for 5,6, and negative

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -4,3 +4,15 @@ from mymath.factorial import factorial
 
 def test_3():
     assert factorial(3) == 6
+    
+def test_5():
+    assert factorial(5) == 120
+    
+def test_6():
+    assert factorial(6) == 720
+    
+def test_negative():
+    with pytest.raises(ValueError):
+        factorial(-1)
+    
+
